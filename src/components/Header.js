d54c7205af7dabf47c62677bcdf34d29d9ed0f9e
Prince Kumar <princesd991@gmail.com>
2023-11-28 13:46:24 +0530

    setup Orionqo

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import banner from "../assets/img/Doctor banner.jpg"
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <img src={banner} alt="" width="100%"  height="300px" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
