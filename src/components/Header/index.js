854c9cd5564a473fd9d6f8ce26b718297448cef1
Prince Kumar <princesd991@gmail.com>
2023-11-29 15:10:50 +0530

    build the banner

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,23 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import logo from "../../assets/img/Orionqo.png"
-import './index.css'
-import { faMagnifyingGlass, faUser } from "@fortawesome/free-solid-svg-icons"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import logo from "../../assets/img/Orionqo.png";
+import "./index.css";
+import { faMagnifyingGlass, faUser } from "@fortawesome/free-solid-svg-icons";
+import Logo from "../../assets/img/Orionqo 2.png";
 
 const Header = () => {
-   return (
+  return (
     <>
-    <section>
+     <section>
+        <div id="navbar">
+          <div id="header-image">
+            <div className="hero-text">
+              <img src={Logo} alt="" width="200" />
+            </div>
+          </div>
+        </div>
+      </section>
+      
+    {/* <section>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid justify-space-between">
           <span classNameName="navbar-brand">
@@ -36,7 +46,7 @@ const Header = () => {
                     Home
                   </a>
                 </li> */}
-                {/* <li className="nav-item">
+      {/* <li className="nav-item">
                   <a className="nav-link " aria-current="page" href="#">
                     Hospitals
                   </a>
@@ -45,7 +55,7 @@ const Header = () => {
                   <a className="nav-link " aria-current="page" href="#">
                     Doctors
                   </a>
-                </li> */}
+                </li> 
               </ul>
 
               <span><FontAwesomeIcon icon={faUser} /></span>
@@ -54,7 +64,7 @@ const Header = () => {
               <form className="d-flex" role="search">
                <input className="form-control me-2" id='navSearchbox' type="search" placeholder="Hospitals, Doctors, diease" aria-label="Search" /> 
                <span><FontAwesomeIcon icon={faMagnifyingGlass} /> </span>
-               {/* <button className="btn btn-outline-success" type="submit">Search</button> */}
+               {/* <button className="btn btn-outline-success" type="submit">Search</button> 
               </form>
               </span>
 
@@ -62,115 +72,15 @@ const Header = () => {
                 <button type="button" class="btn btn-primary">
                   Login
                 </button>
-              </span> */}
+              </span>
             </div>
           </span>
         </div>
       </nav>
     </section>
-     
-     <section>
-      <div>
-        <div id='header-image'></div>
-      </div>
-     </section>
+      */}
     </>
+  );
+};
 
-    // <nav className="nav-header">
-    //   <div className="nav-content">
-    //     <div className="nav-bar-mobile-logo-container">
-    //       <Link to="/">
-    //         <img
-    //           className="website-logo"
-    //           src={logo}
-    //           alt="website logo"
-    //         />
-    //       </Link>
-
-    //       <button type="button" className="nav-mobile-btn">
-    //         <img
-    //           src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
-    //           alt="nav logout"
-    //           className="nav-bar-image"
-             
-    //         />
-    //       </button>
-    //     </div>
-
-    //     <div className="nav-bar-large-container">
-    //       <Link to="/">
-    //         <img
-    //           className="website-logo"
-    //           src={logo}
-    //           alt="website logo"
-    //         />
-    //       </Link>
-    //       <ul className="nav-menu">
-    //         <li className="nav-menu-item">
-    //           <Link to="/" className="nav-link">
-    //             Home
-    //           </Link>
-    //         </li>
-
-    //         <li className="nav-menu-item">
-    //           <Link to="/products" className="nav-link">
-    //             Hospitals
-    //           </Link>
-    //         </li>
-
-    //         <li className="nav-menu-item">
-    //           <Link to="/cart" className="nav-link">
-    //             Doctors
-    //           </Link>
-    //         </li>
-    //       </ul>
-    //       <button
-    //         type="button"
-    //         className="logout-desktop-btn"
-           
-    //       >
-    //         Login
-    //       </button>
-    //     </div>
-    //   </div>
-    //   <div className="nav-menu-mobile">
-    //     <ul className="nav-menu-list-mobile">
-    //       <li className="nav-menu-item-mobile">
-    //         <Link to="/" className="nav-link">
-    //           <img
-    //             src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
-    //             alt="nav home"
-    //             className="nav-bar-image"
-    //           />
-    //         </Link>
-    //       </li>
-
-    //       <li className="nav-menu-item-mobile">
-    //         <Link to="/products" className="nav-link">
-    //           <img
-    //             src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
-    //             alt="nav products"
-    //             className="nav-bar-image"
-    //           />
-    //         </Link>
-    //       </li>
-    //       <li className="nav-menu-item-mobile">
-    //         <Link to="/cart" className="nav-link">
-    //           <img
-    //             src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
-    //             alt="nav cart"
-    //             className="nav-bar-image"
-    //           />
-    //         </Link>
-    //       </li>
-    //     </ul>
-    //   </div>
-    // </nav>
-  )
-}
-
-export default Header
-
-
-
-
+export default Header;
