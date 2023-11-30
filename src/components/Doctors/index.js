dfd053a82eaa7443a9783774fc44fa624b61b9fc
Prince Kumar <princesd991@gmail.com>
2023-11-30 12:56:00 +0530

    call Doctor Data on UI and Doctor Json

--- src/components/Doctors/index.js
+++ src/components/Doctors/index.js
@@ -1,68 +1,70 @@
 import React from "react";
-import HospitalData from "../../Data/HospitalData.json"
-import DoctorImg from "../../assets/img/doctor.png"
+import DoctorData from "../../Data/Doctors.json";
+import DoctorImg from "../../assets/img/doctor.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const Doctors = () => {
-
-
-    // const {Doctors} = HospitalData;
+  const { doctors } = DoctorData;
 
   return (
     <>
       <div>
         <div className="d-flex justify-content-between">
-            <span>
-                <p>All Doctors (30)</p>
-            </span>
-            <span>
-                <input type="search" width="200" placeholder="Search Doctor, Diesases" name="" id="" />
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-            </span>
+          <span>
+            <p>All Doctors (30)</p>
+          </span>
+          <span>
+            <input
+              type="search"
+              width="200"
+              placeholder="Search Doctor, Diesases"
+              name=""
+              id=""
+            />
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </span>
         </div>
+
 
         {/* All Doctors here */}
-
-        <div>
-      {/* <h2>{hospital_name}</h2> */}
-
-    {/*  <h3>Doctors:</h3>
-       <ul>
-        {HospitalData.AllDoctors.map(doctorGroup => (
-          doctorGroup.doctors.map(doctor => (
-            <li key={doctor.doctor_id}>
-              <p>{`${doctor.first_name} ${doctor.last_name}`}</p>
-              <p>Email: {doctor.email}</p>
-              <p>Contact: {doctor.contact}</p>
-              <p>Specialty: {doctor.specialty}</p>
-            </li>
-          ))
-        ))}
-      </ul> */}
-    </div>
-        {/* {Doctors.map(eachItem=>( */}
-        <div>
-          <div class="card">
-            <img src={DoctorImg} width="80px" height="80px" className="" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Dctor Name</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-              <span>
-              <a href="#" className="btn btn-primary">
-                Full Profle
-              </a>
-              <a href="#" className="btn btn-primary">
-                Appointment Book
-              </a>
-              </span>
+        {doctors.map((eachItem) => (
+          <div className="mt-2">
+            <div class="card">
+              <img
+                src={DoctorImg}
+                width="80px"
+                height="80px"
+                className=""
+                alt="..."
+              />
+              <div class="card-body">
+                <h5 class="card-title">{eachItem.first_name} {eachItem.last_name}</h5>
+                <p class="card-text">
+                  {eachItem.profile_image}
+                </p>
+                <p>
+                  {eachItem.hospital_name}
+                </p>
+                <p>
+                  {eachItem.email}
+                </p>
+                <p>
+                  {eachItem.contact}
+                </p>
+                  {eachItem.specialty}
+                <span>
+                  <button href="#" className="btn btn-primary">
+                    Full Profle
+                  </button>
+                  <button href="#" className="btn btn-primary">
+                    Appointment Book
+                  </button>
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-        {/* )) } */}
+        ))}
       </div>
     </>
   );
